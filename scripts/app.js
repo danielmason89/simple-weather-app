89e6c305d81924997201252279c28fa2c579b139
Daniel Mason <danmasson0@gmail.com>
2022-11-12 22:41:49 -0500

    refactored path for day/night svg

--- scripts/app.js
+++ scripts/app.js
@@ -19,7 +19,7 @@ const updateUI = (data) => {
   const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
   icon.setAttribute("src", iconSrc);
 
-  let timeSrc = weather.IsDayTime ? "../img/day.svg" : "../img/night.svg";
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
   time.setAttribute("src", timeSrc);
 
   if (card.classList.contains("d-none")) {
